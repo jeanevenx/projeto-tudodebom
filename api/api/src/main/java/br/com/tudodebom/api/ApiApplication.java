f3a24d1200943906bca5f94c6be8d4d41355226c
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2022-08-15 07:50:04 -0300

    Update ApiApplication.java

--- api/api/src/main/java/br/com/tudodebom/api/ApiApplication.java
+++ api/api/src/main/java/br/com/tudodebom/api/ApiApplication.java
@@ -9,9 +9,6 @@ import br.com.tudodebom.api.model.Produto;
 public class ApiApplication {
 
 	public static void main(String[] args) {
-		Produto test = new Produto();
-		
-		System.out.println(test.getTipo());
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
