085ce41e5a127fd50e4980b3d60c2fd66e12c099
Wegelys <81177882+Wegelys@users.noreply.github.com>
2022-08-15 12:23:54 -0300

    atualização das classes pedidos, mapeamento com banco

--- api/api/src/main/java/br/com/tudodebom/api/model/Pedido.java
+++ api/api/src/main/java/br/com/tudodebom/api/model/Pedido.java
@@ -26,16 +26,23 @@ public class Pedido {
 	    @Id
 	    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	    private Long id;
-
+	    
 	    @Column(name = "data_transacao")
 	    private LocalDateTime data_transacao;
 	    
 	    @Column(name = "status")
 	    private LocalDateTime status;
-
-	    @Column(name = "valor")
-	    private Double valor;
+	    
+	    @Column(name = "valor_bruto")
+	    private Double valor_Bruto;
+	    
+	    @Column(name = "desconto")
+	    private Double desconto;
+	    
+	    @Column(name = "valor_final")
+	    private Double valor_final;
 	    // PARA CADA COMPRA (( UM CLIENTE COMPROU ), LIGAÇÃO COM TABELA CLIENTE)
+	    
 	    @ManyToOne
 	    private Cliente pedido;
 
