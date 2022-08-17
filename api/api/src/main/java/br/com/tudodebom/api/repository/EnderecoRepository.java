8ce50053bd9d5e76ed61f09475aa0715261e4784
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2022-08-17 05:12:42 -0300

    Delete EnderecoRepository.java

--- api/api/src/main/java/br/com/tudodebom/api/repository/EnderecoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.tudodebom.api.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.com.tudodebom.api.model.Endereco;
-
-public interface EnderecoRepository extends CrudRepository<Endereco,Integer> {
-
-}
